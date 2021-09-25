5a14c526d06b97bf3f52fa8730761abefde28158
Austin <austephner@gmail.com>
2021-09-25 00:50:46 -0400

    Initial Check In

---
+++ Editor/RandomizableAttributePropertyDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RandomizableIntFields.Editor
+{
+    [CustomPropertyDrawer(typeof(RandomizableAttribute))]
+    public class RandomizableAttributePropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var randomizableAttribute = (RandomizableAttribute)attribute;
+
+            if (fieldInfo.FieldType.IsGenericType)
+            {
+                position.width -= 30;
+                EditorGUI.PropertyField(position, property, label);
+            }
+            else
+            {
+                // todo: show warning icon
+                GUI.Label(position, new GUIContent("Type Not Supported"));
+                return;
+            }
+
+            position.x += position.width + 5;
+            position.width = 25;
+
+            if (GUI.Button(position, "R"))
+            {
+                var genericType = fieldInfo.FieldType.GetGenericTypeDefinition();
+
+                if (genericType == typeof(float))
+                {
+                    property.floatValue = Random.Range(randomizableAttribute.min, randomizableAttribute.max);
+                }
+                else if (genericType == typeof(int)) 
+                {
+                    property.intValue = Mathf.RoundToInt(Random.Range(randomizableAttribute.min, randomizableAttribute.max));
+                }
+            }
+        }
+    }
+}
---
+++ RandomizableAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace RandomizableIntFields
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class RandomizableAttribute : PropertyAttribute
+    {
+	    private float _min = 0, _max = 0;
+
+        public float min => _min;
+
+        public float max => _max;
+
+        public RandomizableAttribute(float max)
+        {
+	        this.
+	        _max = max;
+        }
+
+        public RandomizableAttribute(float min, float max)
+        {
+	        _min = min;
+	        _max = max;
+        }
+    }
+}
