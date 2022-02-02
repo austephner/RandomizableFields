b74ea34b310a7803d5443c13d523a10cb0b032a6
Austin <austephner@gmail.com>
2022-02-02 16:44:28 -0500

    Including meta files for package

---
+++ Samples/Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace RandomizableFields.Samples
+{
+    public class Example : MonoBehaviour
+    {
+        [Randomizable(0.0f, 1.0f)] [SerializeField] 
+        private float _randomFloat; 
+
+        [Randomizable(0, 100)] [SerializeField] 
+        private int _randomInt;
+
+        [RandomizableGuid] [SerializeField] 
+        private string _randomGuid = Guid.NewGuid().ToString();
+    }
+}
